ce5e2539b01922c77c3cc6ddd9b8e2c1c4392fb8
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-01-07 22:14:56 -0500

    Update TeamFights.php

--- src/vale/TeamFights.php
+++ src/vale/TeamFights.php
@@ -1,7 +1,52 @@
 <?php
 
 namespace vale;
+
+use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
-class TeamFights extends PluginBase implements Listener{}
+class TeamFights extends PluginBase implements Listener{
+
+  /** @var array $readTeam*/
+  public static $redTeam = [];
+  
+    /** @var array blueTeam*/
+  public static $blueTeam = [];
+  
+  
+  public const START = "Started";
+  
+  public const NOT_ENOUGH_PLAYERS = "must start with 4 players";
+  
+  public static $gameTime = 600;
+  
+  /**var @array $arenas*/
+  
+  public static $arenas = ["map1"];
+  
+  private static $instance;
+  
+  
+  
+  
+  public function onEnable(): void{
+  self::$instance = $this;
+   @mkdir($this->getDataFolder();
+
+foreach(self::$worlds as $world){
+
+if(!$this->getServer()->isLevelLoaded($world){
+
+$this->getServer()->loadLevel($world);
+
+  
+}
+   }
+   }
+
+  
+  
+
+
+}
